6fb50fbf8da8bb51b8fc30def7cf6851997afc00
Ubu <ubald2b@gmail.com>
2019-07-18 00:49:28 +0200

    Added project name field in form component

--- src/form/form.js
+++ src/form/form.js
@@ -6,7 +6,9 @@ class Form extends HTMLElement {
         return 'gjh-form'
     }
     connectedCallback() {
-        this.innerHTML = `<gjh-form-field label="Github API URL"/>`
+        this.innerHTML = `
+            <gjh-form-field data-test="api-url" label="Github API URL"></gjh-form-field>
+            <gjh-form-field data-test="projectName" label="Project name"></gjh-form-field>`
     }
 }
 
--- src/form/form.spec.js
+++ src/form/form.spec.js
@@ -7,9 +7,15 @@ describe('Form component', () => {
 
     describe('Initialization', () => {
         it('should display a field to fill github api', () => {
-            const githubApiField = component.querySelector('gjh-form-field')
+            const githubApiField = component.querySelector('[data-test=api-url]')
 
             expect(githubApiField.getAttribute('label')).to.equals('Github API URL')
+        });
+
+        it('should display a field to fill project name', () => {
+            const projectName = component.querySelector('[data-test=projectName]')
+
+            expect(projectName.getAttribute('label')).to.equals('Project name')
         });
     });
 });
