76df641930183f3cfb23f0446d26412344bed820
Ubu <ubald2b@gmail.com>
2019-07-19 00:12:48 +0200

    Added commit list component

---
+++ src/commit-list/commit-list.js
@@ -0,0 +1,29 @@
+import {declareWebComponent} from '../utils.js'
+
+class CommitList extends HTMLElement {
+    static get tagName() {
+        return 'gjh-commit-list'
+    }
+
+    set list(list) {
+        this._list = list
+        this.render()
+    }
+
+    render() {
+        const commits = this._list
+
+        const commitList = commits => commits
+            .map(message => `<li>${message}</li>`)
+            .reduce((acc, next) => acc + next)
+
+        this.innerHTML = Object.entries(commits)
+            .map(([key, commits]) => `<div>
+                    <span data-test="list-label">${key}</span>
+                    <ul data-test="list-commits">${commitList(commits)}</ul>
+                </div>`)
+            .reduce((acc, next) => acc + next)
+    }
+}
+
+declareWebComponent(CommitList)
---
+++ src/commit-list/commit-list.spec.js
@@ -0,0 +1,33 @@
+describe('CommitList component', () => {
+    let component
+    beforeEach(() => {
+        const list = {
+            "unassigned": [
+                "First simple component with dev environment and tests",
+            ],
+            "TEST-123": [
+                "Created form-field component"
+            ]
+        }
+
+        component = document.createElement('gjh-commit-list')
+        component.list = list
+        document.querySelector('body').append(component)
+    });
+
+    describe('Initialization', () => {
+        it('should display labels for the lists', () => {
+            const listLabels = component.querySelectorAll('[data-test=list-label]')
+
+            expect(listLabels[0].textContent).to.equals('unassigned')
+            expect(listLabels[1].textContent).to.equals('TEST-123')
+        });
+
+        it('should display all commits in a list', () => {
+            const commitList = component.querySelectorAll('[data-test=list-commits]')
+
+            expect(commitList[0].textContent).to.equals("First simple component with dev environment and tests")
+            expect(commitList[1].textContent).to.equals("Created form-field component")
+        });
+    });
+});
