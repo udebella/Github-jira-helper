dcbe6565a230ea1d689c252a0ac82f964cc0a542
Ubu <ubald2b@gmail.com>
2019-07-25 00:14:01 +0200

    Keep test running even when there is linting errors

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -2,69 +2,72 @@
 // Generated on Tue Jul 16 2019 23:46:49 GMT+0200 (GMT+02:00)
 
 module.exports = function(config) {
-  config.set({
+	config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '..',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai', 'sinon', 'sinon-chai'],
+		// base path that will be used to resolve all patterns (eg. files, exclude)
+		basePath: '..',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      {pattern: 'src/**/*.js', type: 'module'}
-    ],
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		frameworks: ['mocha', 'chai', 'sinon', 'sinon-chai'],
 
 
-    // list of files / patterns to exclude
-    exclude: [
-    ],
+		// list of files / patterns to load in the browser
+		files: [
+			{pattern: 'src/**/*.js', type: 'module'}
+		],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-	    'src/**/*.js': ['eslint']
-    },
+		// list of files / patterns to exclude
+		exclude: [],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {
+			'src/**/*.js': ['eslint']
+		},
 
 
-    // web server port
-    port: 9876,
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: ['progress'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+		// web server port
+		port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['FirefoxHeadless'],
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers: ['FirefoxHeadless'],
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity,
-  })
+
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: false,
+
+		// Concurrency level
+		// how many browser should be started simultaneous
+		concurrency: Infinity,
+
+		eslint: {
+			stopOnError: false
+		}
+	})
 }
